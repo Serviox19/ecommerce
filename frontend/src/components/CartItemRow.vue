21ea830ee759163962b5b52d783e3b7b206de4b5
servio <servio.mora19@gmail.com>
2020-02-20 17:23:57 -0500

    cart template and set up component for dynamic items

---
+++ frontend/src/components/CartItemRow.vue
@@ -0,0 +1,48 @@
+<template>
+  <tr class="cart__row responsive-table__row">
+    <td class="cart__table-cell--image small--text-center">
+      <div id="CartImageWrapper--" class="cart__image-wrapper supports-js">
+        <a class="cart__image-container" href="" style="padding-top:100.0%;">
+          <img id="CartImage--" class="cart__image lazyautosizes lazyloaded" src="//cdn.shopify.com/s/files/1/0037/5472/products/SGT-590A_01_grande_372def63-6e61-4087-a210-36c0cc682683_100x100.jpg?v=1569049261" data-widths="[180, 230, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048]" data-aspectratio="1.0" data-sizes="auto" alt="alt here" itemprop="image" />
+        </a>
+      </div>
+    </td>
+    <td class="cart__table-cell--meta small--text-center">
+      <p>
+        <a href="">Roland Flat Peak Cap - Black/Black</a>
+        <br><small>Black/Black / OSFA</small>
+      </p>
+      <p class="txt--minor">
+        <a href="" class="cart__remove">Remove</a>
+      </p>
+    </td>
+    <td class="cart__table-cell--price medium-up--text-center" data-label="Price">
+      <span>$399.95</span>
+    </td>
+    <td data-label="Quantity" class="medium-up--text-center cart__table-cell--quantity">
+      <label class="visually-hidden" for="">Quantity</label>
+      <input class="quantity-selector" type="number" name="" id="" value="1" min="0">
+    </td>
+    <td data-label="Total" class="text-right cart__table-cell--line-price">
+      <span>$399.95</span>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'cart-item',
+  components: {},
+  props: [],
+  data() {
+    return {}
+  },
+  created() {
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- frontend/src/views/Cart.vue
+++ frontend/src/views/Cart.vue
@@ -12,27 +12,84 @@
     </nav>
     <!-- /templates/cart.liquid -->
     <div class="grid">
-      <div class="grid__item">
-        <div id="EmptyCart">
+      <div class="grid__item" style="padding:0">
+        <div id="EmptyCart" v-if="!cart.length">
           <h1>Shopping Cart</h1>
           <p class="cart--empty-message">Your cart is currently empty.</p>
           <p class="cart--continue-message">Continue browsing <a href="/collections">here</a>.</p>
           <p class="cart--cookie-message">Enable cookies to use the shopping cart</p>
         </div>
+
+        <table class="responsive-table table--no-border" v-else>
+          <thead class="cart__row cart__header-labels small--hide">
+            <tr>
+              <th class="text-left cart__table-cell--image">Product</th>
+              <th class="text-center cart__table-cell--meta"></th>
+              <th class="text-center cart__table-cell--price">Price</th>
+              <th class="text-center cart__table-cell--quantity">Quantity</th>
+              <th class="text-right cart__table-cell--line-price">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- Loop for each product -->
+            <CartItemRow />
+          </tbody>
+        </table>
+
+        <footer class="cart__footer">
+          <div class="grid">
+            <div class="grid__item medium-up--one-half">
+              <label for="CartSpecialInstructions" class="label--block">Special instructions for seller</label>
+              <textarea name="note" id="CartSpecialInstructions" class="input--block cart__note"></textarea>
+            </div>
+            <div class="grid__item text-center medium-up--one-half medium-up--text-right">
+              <p>
+                <span class="cart__subtotal-title h3">Subtotal</span>
+                <span class="cart__subtotal h3">$399.95</span>
+              </p>
+
+              <p class="cart__policies txt--emphasis rte">Taxes and shipping calculated at checkout
+              </p>
+              <p>
+                <button type="submit" name="update" class="btn btn--secondary update-cart">
+                  <svg aria-hidden="true" focusable="false" role="presentation" class="icon icon-update" viewBox="0 0 25 25">
+                    <path
+                      d="M22.339 1.386a1.606 1.606 0 0 0-1.743 1.458l-.077.875A12.124 12.124 0 0 0 12.211.456C5.478.456 0 5.933 0 12.666s5.478 12.212 12.211 12.212c4.653 0 8.84-2.587 10.923-6.748a1.606 1.606 0 1 0-2.872-1.438 8.959 8.959 0 0 1-8.051 4.973c-4.962 0-8.999-4.035-8.999-8.998 0-4.961 4.037-8.999 8.999-8.999a8.93 8.93 0 0 1 6.172 2.452l-1.292-.115a1.604 1.604 0 0 0-1.741 1.458 1.605 1.605 0 0 0 1.457 1.742l4.799.426a1.601 1.601 0 0 0 1.742-1.458l.448-5.045a1.607 1.607 0 0 0-1.457-1.742z">
+                    </path>
+                  </svg>
+                  <span class="icon__fallback-text">Update Cart</span>
+                </button>
+                <a class="btn btn--secondary" href="">Continue shopping</a>
+              </p>
+              <p>
+                <button type="submit" name="checkout" class="btn">Check Out</button>
+              </p>
+            </div>
+          </div>
+        </footer>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import CartItemRow from '@/components/CartItemRow'
+
 export default {
   name: 'cart',
-  components: {},
+  components: {
+    CartItemRow
+  },
   data() {
+    return {
+      cart: this.$store.state.cart
+    }
   },
   created() {
   },
   mounted() {
+    console.log(this.$store.state.cart);
   },
   methods: {}
 }
